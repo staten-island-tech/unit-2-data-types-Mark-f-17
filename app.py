ca3211ed1670f87f0507b51d80aaa6909d22a188
Mark-f-17 <Markf59@nycstudents.net>
2024-09-17 10:14:00 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+""" x = 3
+y = float(3)
+print(x, y) """
+
+""" values = [1, 2, 5, 7, 8, 24]
+print(values)
+for i in values:
+    print(i) """
+
+""" x = "mark f"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+right_number = input("what number is it")
+if right_number == "19":
+    print("correct")
+else:
+    print("incorrct")
+right_number = "19"
