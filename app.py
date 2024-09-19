4f975798edc3e763ba8290b1c923202af0cce2cb
Mark-f-17 <Markf59@nycstudents.net>
2024-09-19 10:11:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,20 @@ else:
 """ x = "test"
 print(f"hello {x}") """
 
-number = int(input("even or odd "))
+""" number = int(input("even or odd "))
 if (number % 2) == (0):
     print("even")
 if (number % 2) == (1):
     print("odd")
+ """
+
+tip = input("how was your excperience? ")
+bill = float(input("what is your total?"))
+if tip == "bad":
+    print("your total is", bill)
+elif tip == "ok":
+    print("your total is", round(bill * 1.15, 2))
+elif tip == "good":
+    print("your total is", round(bill * 1.20, 2))
+elif tip == "great":
+    print("your total is", round(bill * 1.25, 2))
